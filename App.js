90b73bc9716b8f14de66b330a83911b9237df82f
heuui <henni.haavistola@gmail.com>
2022-08-31 10:01:31 +0300

    toiminnallisuuksien lisääminen

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, StyleSheet, Text, Button, TextInput, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      const [text, setText] = useState('');
+
+      const buttonPressed = () => {
+        Alert.alert(text + ' to you too!');
+      }
+
+        return (
+          <View style={styles.container}>
+            <Text>Say something to the World!</Text>
+            <TextInput
+              style={{ borderColor: 'gray', borderWidth: 1, width: 200, height: 50 }}
+              onChangeText={text => setText(text)}
+              value={text}
+            />
+            <Button 
+              onPress={buttonPressed} title="Enter" 
+              />
+            <StatusBar style="auto" />
+          </View>
+        );
+      }
+
+      const styles = StyleSheet.create({
+        container: {
+          flex: 1,
+          backgroundColor: '#fff',
+          alignItems: 'center',
+          justifyContent: 'space-evenly',
+        },
+      });
